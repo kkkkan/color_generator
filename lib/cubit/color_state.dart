bc702e442441f3574cf5e44c128be4f8aaee9862
kkkkan <kan4649kan@gmail.com>
2021-01-08 13:33:43 +0900

    fix : 起動時の初期値のalphaが0だったので、0xFFになるようにした

--- lib/cubit/color_state.dart
+++ lib/cubit/color_state.dart
@@ -2,7 +2,7 @@ part of 'color_cubit.dart';
 
 @freezed
 abstract class ColorState with _$ColorState {
-  const factory ColorState.initial([@Default(Color(0xFFFFFFFF)) Color color]) =
+  const factory ColorState.initial([@Default(Color.fromARGB(255,255,255,255)) Color color]) =
       _Initial;
   const factory ColorState.color(Color color) = _Changed;
 }
